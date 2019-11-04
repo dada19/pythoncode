20ab9859dfdbfe2de89415e40d80f2e27c6b4ce6
dada19 <ostap.com@gmail.com>
2019-11-04 21:25:15 +0200

    first commit

---
+++ Pyt.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python
+import sys
+
+x = int(sys.argv[1]) # аргументы передаются в cmd после названия вызываемого файла >cd C:\Users\Андрей\Desktop
+y = int(sys.argv[2]) # C:\Users\Андрей\Desktop>Pyt.py 5 5
+print(x ** y)
+print('Ура, сработало!')
